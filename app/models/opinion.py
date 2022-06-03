46aefd0ac33be1e1a976982e50718bf7cf5d5fb7
TymoteuszPolanski <s222733@student.uek.krakow.pl>
2022-06-03 13:00:40 +0200

    2022-06-03 TP

---
+++ app/models/opinion.py
@@ -0,0 +1,4 @@
+class Opinion():
+    def __init__(): #napisanie konstruktorów dla opini i productu
+        pass
+        
---
+++ app/models/product.py
@@ -0,0 +1,4 @@
+class Product():
+    def __init__(): #napisanie konstruktorów dla opini i productu
+        pass
+        
