6278dd0b0b3be7b35b09ceb73a604c3a29b10563
TymoteuszPolanski <s222733@student.uek.krakow.pl>
2022-06-09 20:10:09 +0200

    2022-06-09 TP HOMEWORK

--- app/models/opinion.py
+++ app/models/opinion.py
@@ -1,4 +1,13 @@
 class Opinion():
-    def __init__(): #napisanie konstruktorów dla opini i productu
-        pass
-        
+    def __init__(self, author, recommendation, stars, content, useful, useless, published, purchased, pros, cons, opinion_id):
+        self.author=author
+        self.recommendation=recommendation
+        self.stars=stars
+        self.content=content
+        self.useful=useful
+        self.useless=useless
+        self.published=published
+        self.purchased=purchased
+        self.pros=pros
+        self.cons=cons
+        self.opinion_id=opinion_id
--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,8 @@
 class Product():
-    def __init__(): #napisanie konstruktorów dla opini i productu
-        pass
-        
+    def __init__(self, opinions_number, cons_number, pros_number, average_stars, download):
+
+        self.opinions_number=opinions_number
+        self.cons_number=cons_number
+        self.pros_number=pros_number
+        self.average_stars=average_stars
+        self.download=download
